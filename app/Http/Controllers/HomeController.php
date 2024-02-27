49af4a00e6e16505ed5ab0f1d3ed20d541546ca6
Ranjeet Poojari <ranjeetpoojari77@gmail.com>
2024-02-27 12:41:14 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        return view('index');
+    }
+    public function about(){
+        return view('about');
+    }
+    public function contact(){
+        return view('contact');
+    }
+    public function orderlist(){
+        return view('order');
+    }
+    public function orderdetail(){
+        return view('orderdetail');
+    }
+    public function privacy(){
+        return view('privacy');
+    }
+    public function termscondition(){
+        return view('termscondition');
+    }
+    public function blog(){
+        return view('blog');
+    }
+    public function blogdetail(){
+        return view('blogdetail');
+    }
+}
